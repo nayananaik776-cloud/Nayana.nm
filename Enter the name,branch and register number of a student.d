b5646e2938ecf1179155ebc1a50e848b6cbc91d4
nayananaik776-cloud <nayananaik776@gmail.com>
2025-11-02 13:44:42 +0530

    Create Enter the name,branch and register number of a student.d

---
+++ Enter the name,branch and register number of a student.d
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a,c;
+    char b;
+    printf("enter your name:\n");
+    scanf("%d",&a);
+    printf("enter your branch:\n");
+    scanf("%s",&b);
+    printf("enter your register number:\n");
+    scanf("%d",&c);
+    return 0;
+}
+
+OUTPUT
+enter your name:
+Nayana mohan
+enter your branch:BTech in computer science
+enter your register number:25BTCE133
+
+
+=== Code Execution Successful ===
+
+
